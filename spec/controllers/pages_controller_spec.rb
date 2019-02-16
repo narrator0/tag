637eda476c3c9c871b299329d3fad8221adad821
arfullight <tmimvhuang@gmail.com>
2019-02-16 01:32:17 -0800

    create test case for homepage

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-
+  describe 'GET #index' do
+    it 'response  200' do
+      get :index
+      expect(response).to have_http_status(200)
+    end
+  end
 end
